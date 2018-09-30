58a2f9dfcc0f689463b8b14204aa835fa1752ffb
akash <aakashsingh@gmail.com>
2018-09-30 22:05:53 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,308 @@
+
+from keras.layers import Input,Dropout,BatchNormalization,Activation,Add
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras import backend as K
+
+import numpy as np
+
+import tensorflow as tf
+
+
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    labels = y_true_in
+    y_pred = y_pred_in
+
+    true_objects = 2
+    pred_objects = 2
+
+    #  if all zeros, original code  generate wrong  bins [-0.5 0 0.5],
+    temp1 = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=([0, 0.5, 1], [0, 0.5, 1]))
+    #     temp1 = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))
+    # print(temp1)
+    intersection = temp1[0]
+    # print("temp2 = ",temp1[1])
+    # print(intersection.shape)
+    # print(intersection)
+    # Compute areas (needed for finding the union between all objects)
+    # print(np.histogram(labels, bins = true_objects))
+    area_true = np.histogram(labels, bins=[0, 0.5, 1])[0]
+    # print("area_true = ",area_true)
+    area_pred = np.histogram(y_pred, bins=[0, 0.5, 1])[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:, 1:]
+    intersection[intersection == 0] = 1e-9
+
+    union = union[1:, 1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+
+    # Precision helper function
+    def precision_at(threshold, iou):
+        matches = iou > threshold
+        true_positives = np.sum(matches, axis=1) == 1  # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+
+    # Loop over IoU thresholds
+    prec = []
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        prec.append(p)
+
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+
+
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = y_true_in.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        value = iou_metric(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    return np.mean(metric)
+
+
+def get_iou_vector(A, B):
+    batch_size = A.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        t, p = A[batch] > 0, B[batch] > 0
+        #         if np.count_nonzero(t) == 0 and np.count_nonzero(p) > 0:
+        #             metric.append(0)
+        #             continue
+        #         if np.count_nonzero(t) >= 1 and np.count_nonzero(p) == 0:
+        #             metric.append(0)
+        #             continue
+        #         if np.count_nonzero(t) == 0 and np.count_nonzero(p) == 0:
+        #             metric.append(1)
+        #             continue
+
+        intersection = np.logical_and(t, p)
+        union = np.logical_or(t, p)
+        iou = (np.sum(intersection > 0) + 1e-10) / (np.sum(union > 0) + 1e-10)
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric.append(np.mean(s))
+
+    return np.mean(metric)
+
+
+def my_iou_metric(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred > 0.5], tf.float64)
+
+
+def my_iou_metric_2(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred > 0], tf.float64)
+
+# code download from: https://github.com/bermanmaxim/LovaszSoftmax
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    gts = tf.reduce_sum(gt_sorted)
+    intersection = gts - tf.cumsum(gt_sorted)
+    union = gts + tf.cumsum(1. - gt_sorted)
+    jaccard = 1. - intersection / union
+    jaccard = tf.concat((jaccard[0:1], jaccard[1:] - jaccard[:-1]), 0)
+    return jaccard
+
+
+# --------------------------- BINARY LOSSES ---------------------------
+
+def lovasz_hinge(logits, labels, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        def treat_image(log_lab):
+            log, lab = log_lab
+            log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
+            log, lab = flatten_binary_scores(log, lab, ignore)
+            return lovasz_hinge_flat(log, lab)
+        losses = tf.map_fn(treat_image, (logits, labels), dtype=tf.float32)
+        loss = tf.reduce_mean(losses)
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+
+    def compute_loss():
+        labelsf = tf.cast(labels, logits.dtype)
+        signs = 2. * labelsf - 1.
+        errors = 1. - logits * tf.stop_gradient(signs)
+        errors_sorted, perm = tf.nn.top_k(errors, k=tf.shape(errors)[0], name="descending_sort")
+        gt_sorted = tf.gather(labelsf, perm)
+        grad = lovasz_grad(gt_sorted)
+        loss = tf.tensordot(tf.nn.relu(errors_sorted), tf.stop_gradient(grad), 1, name="loss_non_void")
+        return loss
+
+    # deal with the void prediction case (only void pixels)
+    loss = tf.cond(tf.equal(tf.shape(logits)[0], 0),
+                   lambda: tf.reduce_sum(logits) * 0.,
+                   compute_loss,
+                   strict=True,
+                   name="loss"
+                   )
+    return loss
+
+
+def flatten_binary_scores(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = tf.reshape(scores, (-1,))
+    labels = tf.reshape(labels, (-1,))
+    if ignore is None:
+        return scores, labels
+    valid = tf.not_equal(labels, ignore)
+    vscores = tf.boolean_mask(scores, valid, name='valid_scores')
+    vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
+    return vscores, vlabels
+
+def lovasz_loss(y_true, y_pred):
+    y_true, y_pred = K.cast(K.squeeze(y_true, -1), 'int32'), K.cast(K.squeeze(y_pred, -1), 'float32')
+    #logits = K.log(y_pred / (1. - y_pred))
+    logits = y_pred #Jiaxin
+    loss = lovasz_hinge(logits, y_true, per_image = True, ignore = None)
+    return loss
+
+
+
+def BatchActivate(x):
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    return x
+
+def convolution_block(x, filters, size, strides=(1,1), padding='same', activation=True):
+    x = Conv2D(filters, size, strides=strides, padding=padding)(x)
+    if activation == True:
+        x = BatchActivate(x)
+    return x
+
+def residual_block(blockInput, num_filters=16, batch_activate = False):
+    x = BatchActivate(blockInput)
+    x = convolution_block(x, num_filters, (3,3) )
+    x = convolution_block(x, num_filters, (3,3), activation=False)
+    x = Add()([x, blockInput])
+    if batch_activate:
+        x = BatchActivate(x)
+    return x
+
+
+# Build model
+def build_Unet_Resnet_custom(input_layer, start_filters, DropoutRatio=0.5):
+    # 101 -> 50
+    conv1 = Conv2D(start_filters * 1, (3, 3), activation=None, padding="same")(input_layer)
+    conv1 = residual_block(conv1, start_filters * 1)
+    conv1 = residual_block(conv1, start_filters * 1, True)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    pool1 = Dropout(DropoutRatio / 2)(pool1)
+
+    # 50 -> 25
+    conv2 = Conv2D(start_filters * 2, (3, 3), activation=None, padding="same")(pool1)
+    conv2 = residual_block(conv2, start_filters * 2)
+    conv2 = residual_block(conv2, start_filters * 2, True)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    pool2 = Dropout(DropoutRatio)(pool2)
+
+    # 25 -> 12
+    conv3 = Conv2D(start_filters * 4, (3, 3), activation=None, padding="same")(pool2)
+    conv3 = residual_block(conv3, start_filters * 4)
+    conv3 = residual_block(conv3, start_filters * 4, True)
+    pool3 = MaxPooling2D((2, 2))(conv3)
+    pool3 = Dropout(DropoutRatio)(pool3)
+
+    # 12 -> 6
+    conv4 = Conv2D(start_filters * 8, (3, 3), activation=None, padding="same")(pool3)
+    conv4 = residual_block(conv4, start_filters * 8)
+    conv4 = residual_block(conv4, start_filters * 8, True)
+    pool4 = MaxPooling2D((2, 2))(conv4)
+    pool4 = Dropout(DropoutRatio)(pool4)
+
+    # Middle
+    convm = Conv2D(start_filters * 16, (3, 3), activation=None, padding="same")(pool4)
+    convm = residual_block(convm, start_filters * 16)
+    convm = residual_block(convm, start_filters * 16, True)
+
+    # 6 -> 12
+    deconv4 = Conv2DTranspose(start_filters * 8, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = concatenate([deconv4, conv4])
+    uconv4 = Dropout(DropoutRatio)(uconv4)
+
+    uconv4 = Conv2D(start_filters * 8, (3, 3), activation=None, padding="same")(uconv4)
+    uconv4 = residual_block(uconv4, start_filters * 8)
+    uconv4 = residual_block(uconv4, start_filters * 8, True)
+
+    # 12 -> 25
+    # deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    deconv3 = Conv2DTranspose(start_filters * 4, (3, 3), strides=(2, 2), padding="valid")(uconv4)
+    uconv3 = concatenate([deconv3, conv3])
+    uconv3 = Dropout(DropoutRatio)(uconv3)
+
+    uconv3 = Conv2D(start_filters * 4, (3, 3), activation=None, padding="same")(uconv3)
+    uconv3 = residual_block(uconv3, start_filters * 4)
+    uconv3 = residual_block(uconv3, start_filters * 4, True)
+
+    # 25 -> 50
+    deconv2 = Conv2DTranspose(start_filters * 2, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = concatenate([deconv2, conv2])
+
+    uconv2 = Dropout(DropoutRatio)(uconv2)
+    uconv2 = Conv2D(start_filters * 2, (3, 3), activation=None, padding="same")(uconv2)
+    uconv2 = residual_block(uconv2, start_filters * 2)
+    uconv2 = residual_block(uconv2, start_filters * 2, True)
+
+    # 50 -> 101
+    # deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    deconv1 = Conv2DTranspose(start_filters * 1, (3, 3), strides=(2, 2), padding="valid")(uconv2)
+    uconv1 = concatenate([deconv1, conv1])
+
+    uconv1 = Dropout(DropoutRatio)(uconv1)
+    uconv1 = Conv2D(start_filters * 1, (3, 3), activation=None, padding="same")(uconv1)
+    uconv1 = residual_block(uconv1, start_filters * 1)
+    uconv1 = residual_block(uconv1, start_filters * 1, True)
+
+    # uconv1 = Dropout(DropoutRatio/2)(uconv1)
+    # output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
+    output_layer_noActi = Conv2D(1, (1, 1), padding="same", activation=None)(uconv1)
+    output_layer = Activation('sigmoid')(output_layer_noActi)
+
+    return output_layer
---
+++ runner.py
@@ -0,0 +1,204 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+import seaborn as sns
+sns.set_style("white")
+
+from sklearn.model_selection import train_test_split
+#from tqdm import tqdm_notebook , tnrange
+from skimage.io import imread, imshow #, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+from keras.preprocessing.image import array_to_img, img_to_array, load_img,save_img
+from keras.models import Model, load_model, save_model
+from keras.layers import Input
+from keras import optimizers
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from models import build_Unet_Resnet_custom, my_iou_metric, my_iou_metric_2, lovasz_loss, iou_metric_batch
+
+import time
+t_start = time.time()
+
+version = 3
+basic_name = 'Unet_resnet_v{version}'
+save_model_entropy_loss_name = basic_name+"_entropy_loss_"+ '.model'
+save_model_lovasz_loss_name = basic_name+"_lovasz_loss_"+ '.model'
+submission_file = basic_name + '.csv'
+
+print(save_model_entropy_loss_name)
+print(save_model_lovasz_loss_name)
+print(submission_file)
+
+img_size_ori = 101
+img_size_target = 101
+data_path = "input/"
+
+def upsample(img):  # not used
+    if img_size_ori == img_size_target:
+        return img
+    return resize(img, (img_size_target, img_size_target), mode='constant', preserve_range=True)
+
+
+def downsample(img):  # not used
+    if img_size_ori == img_size_target:
+        return img
+    return resize(img, (img_size_ori, img_size_ori), mode='constant', preserve_range=True)
+
+
+# Loading of training/testing ids and depths
+train_df = pd.read_csv(data_path+"train.csv", index_col="id", usecols=[0])
+depths_df = pd.read_csv(data_path+"depths.csv", index_col="id")
+train_df = train_df.join(depths_df)
+test_df = depths_df[~depths_df.index.isin(train_df.index)]
+
+train_df["images"] = [np.array(load_img("input/train/images/{}.png".format(idx), grayscale=True)) / 255 for idx
+                      in train_df.index]
+
+train_df["masks"] = [np.array(load_img("input/train/masks/{}.png".format(idx), grayscale=True)) / 255 for idx in
+                     train_df.index]
+
+
+
+#add coverage
+train_df["coverage"] = train_df.masks.map(np.sum) / pow(img_size_ori, 2)
+
+def cov_to_class(val):
+    for i in range(0, 11):
+        if val * 10 <= i:
+            return i
+
+train_df["coverage_class"] = train_df.coverage.map(cov_to_class)
+
+#split_test_train
+ids_train, ids_valid, x_train, x_valid, y_train, y_valid, cov_train, cov_test, depth_train, depth_test = train_test_split(
+    train_df.index.values,
+    np.array(train_df.images.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1),
+    np.array(train_df.masks.map(upsample).tolist()).reshape(-1, img_size_target, img_size_target, 1),
+    train_df.coverage.values,
+    train_df.z.values,
+    test_size=0.2, stratify=train_df.coverage_class, random_state= 1234)
+
+
+#Data augmentation
+x_train = np.append(x_train, [np.fliplr(x) for x in x_train], axis=0)
+y_train = np.append(y_train, [np.fliplr(x) for x in y_train], axis=0)
+print(x_train.shape)
+print(y_valid.shape)
+
+
+#Build Model 1
+input_layer = Input((img_size_target, img_size_target, 1))
+output_layer = build_Unet_Resnet_custom(input_layer, 16,0.5)
+model1 = Model(input_layer, output_layer)
+c = optimizers.adam(lr = 0.01)
+model1.compile(loss="binary_crossentropy", optimizer=c, metrics=[my_iou_metric])
+
+print(model1.summary())
+
+model_checkpoint = ModelCheckpoint(save_model_entropy_loss_name,monitor='my_iou_metric',
+                                   mode = 'max', save_best_only=True, verbose=1)
+reduce_lr = ReduceLROnPlateau(monitor='my_iou_metric', mode = 'max',factor=0.5, patience=5, min_lr=0.0001, verbose=1)
+
+epochs = 50
+batch_size = 32
+history = model1.fit(x_train, y_train,
+                    validation_data=[x_valid, y_valid],
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    callbacks=[ model_checkpoint,reduce_lr],
+                    verbose=2)
+
+# Create Model 2
+model1 = load_model(save_model_entropy_loss_name,custom_objects={'my_iou_metric': my_iou_metric})
+
+# remove layter activation layer and use losvasz loss
+input_x = model1.layers[0].input
+output_layer = model1.layers[-1].input
+model = Model(input_x, output_layer)
+c = optimizers.adam(lr = 0.01)
+
+# lovasz_loss need input range (-∞，+∞), so cancel the last "sigmoid" activation
+# Then the default threshod for pixel prediction is 0 instead of 0.5, as in my_iou_metric_2.
+model.compile(loss=lovasz_loss, optimizer=c, metrics=[my_iou_metric_2])
+
+print(model.summary())
+
+early_stopping = EarlyStopping(monitor='val_my_iou_metric_2', mode = 'max',patience=20, verbose=1)
+model_checkpoint = ModelCheckpoint(save_model_lovasz_loss_name,monitor='val_my_iou_metric_2',
+                                   mode = 'max', save_best_only=True, verbose=1)
+reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric_2', mode = 'max',factor=0.5, patience=5, min_lr=0.0001, verbose=1)
+epochs = 50
+batch_size = 32
+
+history = model.fit(x_train, y_train,
+                    validation_data=[x_valid, y_valid],
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    callbacks=[ model_checkpoint,reduce_lr,early_stopping],
+                    verbose=2)
+
+model = load_model(save_model_lovasz_loss_name,custom_objects={'my_iou_metric_2': my_iou_metric_2,
+                                                   'lovasz_loss': lovasz_loss})
+
+
+def predict_result(model,x_test,img_size_target): # predict both orginal and reflect x
+    x_test_reflect =  np.array([np.fliplr(x) for x in x_test])
+    preds_test = model.predict(x_test).reshape(-1, img_size_target, img_size_target)
+    preds_test2_refect = model.predict(x_test_reflect).reshape(-1, img_size_target, img_size_target)
+    preds_test += np.array([ np.fliplr(x) for x in preds_test2_refect] )
+    return preds_test/2
+
+preds_valid = predict_result(model,x_valid,img_size_target)
+
+
+## Scoring for last model, choose threshold by validation data
+thresholds_ori = np.linspace(0.3, 0.7, 31)
+# Reverse sigmoid function: Use code below because the  sigmoid activation was removed
+thresholds = np.log(thresholds_ori/(1-thresholds_ori))
+
+# ious = np.array([get_iou_vector(y_valid, preds_valid > threshold) for threshold in tqdm_notebook(thresholds)])
+# print(ious)
+ious = np.array([iou_metric_batch(y_valid, preds_valid > threshold) for threshold in thresholds])
+print(ious)
+
+
+# instead of using default 0 as threshold, use validation data to find the best threshold.
+threshold_best_index = np.argmax(ious)
+iou_best = ious[threshold_best_index]
+threshold_best = thresholds[threshold_best_index]
+
+# plt.plot(thresholds, ious)
+# plt.plot(threshold_best, iou_best, "xr", label="Best threshold")
+# plt.xlabel("Threshold")
+# plt.ylabel("IoU")
+# plt.title("Threshold vs IoU ({}, {})".format(threshold_best, iou_best))
+# plt.legend()
+
+def rle_encode(im):
+    '''
+    im: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = im.flatten(order = 'F')
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+x_test = np.array([(np.array(load_img("../input/test/images/{}.png".format(idx), grayscale = True))) / 255 for idx in test_df.index]).reshape(-1, img_size_target, img_size_target, 1)
+preds_test = predict_result(model,x_test,img_size_target)
+t1 = time.time()
+pred_dict = {idx: rle_encode(np.round(downsample(preds_test[i]) > threshold_best)) for i, idx in enumerate(test_df.index.values)}
+t2 = time.time()
+
+print("Usedtime = {t2-t1} s")
+
+sub = pd.DataFrame.from_dict(pred_dict,orient='index')
+sub.index.names = ['id']
+sub.columns = ['rle_mask']
+sub.to_csv(submission_file)
+
+t_finish = time.time()
+print("Kernel run time = {(t_finish-t_start)/3600} hours")
