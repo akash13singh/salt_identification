d68bfb2a1168ade2719b77055befc281d5021fea
akash <aakashsingh@gmail.com>
2019-06-22 16:34:22 +0530

    resnet_unet_model

---
+++ resnet_unet_model.py
@@ -0,0 +1,279 @@
+from keras.layers import Conv2DTranspose
+from keras.layers import UpSampling2D
+from keras.layers import Conv2D
+from keras.layers import BatchNormalization
+from keras.layers import Activation
+from keras.layers import Concatenate
+from keras.layers import Add, ZeroPadding2D
+
+def handle_block_names_old(stage):
+    conv_name = 'decoder_stage{}_conv'.format(stage)
+    bn_name = 'decoder_stage{}_bn'.format(stage)
+    relu_name = 'decoder_stage{}_relu'.format(stage)
+    up_name = 'decoder_stage{}_upsample'.format(stage)
+    return conv_name, bn_name, relu_name, up_name
+
+
+def Upsample2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
+                     batchnorm=False, skip=None):
+
+    def layer(input_tensor):
+
+        conv_name, bn_name, relu_name, up_name = handle_block_names_old(stage)
+
+        x = UpSampling2D(size=upsample_rate, name=up_name)(input_tensor)
+
+        if skip is not None:
+            x = Concatenate()([x, skip])
+
+        x = Conv2D(filters, kernel_size, padding='same', name=conv_name+'1')(x)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'1')(x)
+        x = Activation('relu', name=relu_name+'1')(x)
+
+        x = Conv2D(filters, kernel_size, padding='same', name=conv_name+'2')(x)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'2')(x)
+        x = Activation('relu', name=relu_name+'2')(x)
+
+        return x
+    return layer
+
+
+def Transpose2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
+                      transpose_kernel_size=(4,4), batchnorm=False, skip=None):
+
+    def layer(input_tensor):
+
+        conv_name, bn_name, relu_name, up_name = handle_block_names_old(stage)
+
+        x = Conv2DTranspose(filters, transpose_kernel_size, strides=upsample_rate,
+                            padding='same', name=up_name)(input_tensor)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'1')(x)
+        x = Activation('relu', name=relu_name+'1')(x)
+
+        if skip is not None:
+            x = Concatenate()([x, skip])
+
+        x = Conv2D(filters, kernel_size, padding='same', name=conv_name+'2')(x)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'2')(x)
+        x = Activation('relu', name=relu_name+'2')(x)
+
+        return x
+    return layer
+
+
+def get_conv_params(**params):
+    default_conv_params = {
+        'kernel_initializer': 'glorot_uniform',
+        'use_bias': False,
+        'padding': 'valid',
+    }
+    default_conv_params.update(params)
+    return default_conv_params
+
+def get_bn_params(**params):
+    default_bn_params = {
+        'axis': 3,
+        'momentum': 0.99,
+        'epsilon': 2e-5,
+        'center': True,
+        'scale': True,
+    }
+    default_bn_params.update(params)
+    return default_bn_params
+
+def handle_block_names(stage, block):
+    name_base = 'stage{}_unit{}_'.format(stage + 1, block + 1)
+    conv_name = name_base + 'conv'
+    bn_name = name_base + 'bn'
+    relu_name = name_base + 'relu'
+    sc_name = name_base + 'sc'
+    return conv_name, bn_name, relu_name, sc_name
+
+def basic_identity_block(filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+
+    def layer(input_tensor):
+        conv_params = get_conv_params()
+        bn_params = get_bn_params()
+        conv_name, bn_name, relu_name, sc_name = handle_block_names(stage, block)
+
+        x = BatchNormalization(name=bn_name + '1', **bn_params)(input_tensor)
+        x = Activation('relu', name=relu_name + '1')(x)
+        x = ZeroPadding2D(padding=(1, 1))(x)
+        x = Conv2D(filters, (3, 3), name=conv_name + '1', **conv_params)(x)
+
+        x = BatchNormalization(name=bn_name + '2', **bn_params)(x)
+        x = Activation('relu', name=relu_name + '2')(x)
+        x = ZeroPadding2D(padding=(1, 1))(x)
+        x = Conv2D(filters, (3, 3), name=conv_name + '2', **conv_params)(x)
+
+        x = Add()([x, input_tensor])
+        return x
+
+    return layer
+
+
+def basic_conv_block(filters, stage, block, strides=(2, 2)):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+
+    def layer(input_tensor):
+        conv_params = get_conv_params()
+        bn_params = get_bn_params()
+        conv_name, bn_name, relu_name, sc_name = handle_block_names(stage, block)
+
+        x = BatchNormalization(name=bn_name + '1', **bn_params)(input_tensor)
+        x = Activation('relu', name=relu_name + '1')(x)
+        shortcut = x
+        x = ZeroPadding2D(padding=(1, 1))(x)
+        x = Conv2D(filters, (3, 3), strides=strides, name=conv_name + '1', **conv_params)(x)
+
+        x = BatchNormalization(name=bn_name + '2', **bn_params)(x)
+        x = Activation('relu', name=relu_name + '2')(x)
+        x = ZeroPadding2D(padding=(1, 1))(x)
+        x = Conv2D(filters, (3, 3), name=conv_name + '2', **conv_params)(x)
+
+        shortcut = Conv2D(filters, (1, 1), name=sc_name, strides=strides, **conv_params)(shortcut)
+        x = Add()([x, shortcut])
+        return x
+
+    return layer
+
+
+def usual_conv_block(filters, stage, block, strides=(2, 2)):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+
+    def layer(input_tensor):
+        conv_params = get_conv_params()
+        bn_params = get_bn_params()
+        conv_name, bn_name, relu_name, sc_name = handle_block_names(stage, block)
+
+        x = BatchNormalization(name=bn_name + '1', **bn_params)(input_tensor)
+        x = Activation('relu', name=relu_name + '1')(x)
+        shortcut = x
+        x = Conv2D(filters, (1, 1), name=conv_name + '1', **conv_params)(x)
+
+        x = BatchNormalization(name=bn_name + '2', **bn_params)(x)
+        x = Activation('relu', name=relu_name + '2')(x)
+        x = ZeroPadding2D(padding=(1, 1))(x)
+        x = Conv2D(filters, (3, 3), strides=strides, name=conv_name + '2', **conv_params)(x)
+
+        x = BatchNormalization(name=bn_name + '3', **bn_params)(x)
+        x = Activation('relu', name=relu_name + '3')(x)
+        x = Conv2D(filters*4, (1, 1), name=conv_name + '3', **conv_params)(x)
+
+        shortcut = Conv2D(filters*4, (1, 1), name=sc_name, strides=strides, **conv_params)(shortcut)
+        x = Add()([x, shortcut])
+        return x
+
+    return layer
+
+
+def usual_identity_block(filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+
+    def layer(input_tensor):
+        conv_params = get_conv_params()
+        bn_params = get_bn_params()
+        conv_name, bn_name, relu_name, sc_name = handle_block_names(stage, block)
+
+        x = BatchNormalization(name=bn_name + '1', **bn_params)(input_tensor)
+        x = Activation('relu', name=relu_name + '1')(x)
+        x = Conv2D(filters, (1, 1), name=conv_name + '1', **conv_params)(x)
+
+        x = BatchNormalization(name=bn_name + '2', **bn_params)(x)
+        x = Activation('relu', name=relu_name + '2')(x)
+        x = ZeroPadding2D(padding=(1, 1))(x)
+        x = Conv2D(filters, (3, 3), name=conv_name + '2', **conv_params)(x)
+
+        x = BatchNormalization(name=bn_name + '3', **bn_params)(x)
+        x = Activation('relu', name=relu_name + '3')(x)
+        x = Conv2D(filters*4, (1, 1), name=conv_name + '3', **conv_params)(x)
+
+        x = Add()([x, input_tensor])
+        return x
+
+    return layer
+
+
+def build_unet(backbone, classes, last_block_filters, skip_layers,
+               n_upsample_blocks=5, upsample_rates=(2,2,2,2,2),
+               block_type='upsampling', activation='sigmoid',
+               **kwargs):
+
+    input = backbone.input
+    x = backbone.output
+
+    if block_type == 'transpose':
+        up_block = Transpose2D_block
+    else:
+        up_block = Upsample2D_block
+
+    # convert layer names to indices
+    skip_layers = ([get_layer_number(backbone, l) if isinstance(l, str) else l
+                    for l in skip_layers])
+    for i in range(n_upsample_blocks):
+
+        # check if there is a skip connection
+        if i < len(skip_layers):
+#             print(backbone.layers[skip_layers[i]])
+#             print(backbone.layers[skip_layers[i]].output)
+            skip = backbone.layers[skip_layers[i]].output
+        else:
+            skip = None
+
+        up_size = (upsample_rates[i], upsample_rates[i])
+        filters = last_block_filters * 2**(n_upsample_blocks-(i+1))
+
+        x = up_block(filters, i, upsample_rate=up_size, skip=skip, **kwargs)(x)
+
+    if classes < 2:
+        activation = 'sigmoid'
+
+    x = Conv2D(classes, (3,3), padding='same', name='final_conv')(x)
+    x = Activation(activation, name=activation)(x)
+
+    model = Model(input, x)
+
+    return model
